bdd834c1b9a36bd5ae2ba31c5c09523ddd78d5da
chaoqund <chaoqun_ding@sfu.ca>
2020-07-20 15:00:13 -0700

    Replace Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,102 +1,54 @@
 from imutils.video import VideoStream
 from pyzbar import pyzbar
-import argparse
-import datetime
 import imutils
 import time
 import cv2
-import operator
+
+from DPLSystem.DB.dbHandler import dbHandler
 
 class Scanner():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-o", "--output", type=str, default="barcodes.csv", help="path to output CSV file containing barcodes")
-    args = vars(ap.parse_args())
-    found = dict()
-    csv = open(args["output"], "w")
-    vs = VideoStream(usePiCamera=True).start()
-    time.sleep(1.0)
-    
-    def fromScanner(self, found, vs):
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
+    def __init__(self):
+        self.vs = VideoStream(usePiCamera=True).start()
+        time.sleep(0.5)
 
-        # find the barcodes in the frame and decode each of the barcodes
-        barcodes = pyzbar.decode(frame)
+    def __del__(self):
+        cv2.destroyAllWindows()
 
-        # loop over the detected barcodes
-        for barcode in barcodes:
-            # extract the bounding box location of the barcode and draw
-            # the bounding box surrounding the barcode on the image
-            (x, y, w, h) = barcode.rect
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-            # the barcode data is a bytes object so if we want to draw it
-            # on our output image we need to convert it to a string first
-            barcodeData = barcode.data.decode("utf-8")
-            barcodeType = barcode.type
-            # draw the barcode data and barcode type on the image
-            text = "{} ({})".format(barcodeData, barcodeType)
-            cv2.putText(frame, text, (x, y - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-            # if the barcode text is currently not in our CSV file, write
-            # the timestamp + barcode to disk and update the set
-            if barcodeData not in found:
-                found[barcodeData] = 1
-            
-            if barcodeData in found:
-                found[barcodeData] += 1
-                
-        # show the output frame
-        cv2.imshow("Barcode Scanner", frame)
-        key = cv2.waitKey(1) & 0xFF
-        
-    def endPoint(self, content, phone):
-        self.csv.write("{},{},{}\n".format(datetime.datetime.now(), content, phone))
-        self.csv.flush()
-        self.csv.close()
-        cv2.destroyAllWindows()     
-
-    def startPoint(self):
-        
-        self.found = dict()
-
-        while True:
-            self.fromScanner(self.found, self.vs)
-            if self.found:
-                break
-        
-        # close the output CSV file do a bit of cleanup
-        if self.found:
-            print("All contents: ", self.found)
-            content = max(self.found.items(), key=operator.itemgetter(1))[0]
-            print("Final detected content is: ")
-            print(content)
-            contentCorrect = input("Is this content correct? [y/n] ")
-            while contentCorrect != 'y' and contentCorrect != 'n' and contentCorrect != 'Y' and contentCorrect != 'N':
-                print("invalid input, please try again!")
-                contentCorrect = input("Is this content correct? [y/n] ")
-            if contentCorrect == 'n' or contentCorrect == 'N':
-                enterContent = input("choose the method you would like to enter code contents: 1. scan again 2. user input from keyboard [1/2] ")
-                while enterContent != '1' and enterContent != '2':
-                    print("invalide input, please try again!")
-                    enterContent = input("choose the method you would like to enter code contents: 1. scan again 2. user input from keyboard [1/2] ")
-                if enterContent == '1':
-                    self.startPoint()
-                if enterContent == '2':
-                    content = input("Please enter the correct content: ")
-                    phone = input("Please enter the package receiver's phone number: ")
-                    self.endPoint(content, phone)
-            if contentCorrect == 'y' or contentCorrect == 'Y':
-                phone = input("Please enter the package receiver's phone number: ")
-                self.endPoint(content, phone)
+    def resultInDB(self, barcodeData):
+        dbContent = dbHandler()
+        if dbContent.get_delivery_by_trackingNumber(barcodeData) is not None:
+            return True
         else:
-            print("Scanned result is empty, please scan again")
-            self.startPoint()
-       
+            return False
+
+    def getScannedResult(self):
+        while True:
+            frame = self.vs.read()
+            frame = imutils.resize(frame, width=400)
+            barcodes = pyzbar.decode(frame)
+            for barcode in barcodes:
+                (x, y, w, h) = barcode.rect
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+                text = "{} ({})".format(barcodeData, barcodeType)
+                cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                print("scanned result: ", barcodeData)
+                if self.resultInDB(barcodeData):
+                    print("in database")
+                    break
+                else:
+                    print("scanned barcode data is not in dababase")
+                    continue
+
+
+            cv2.imshow("Barcode Scanner", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+
 
 # uncomment codes below to test this file
-# scan = Scanner()
-# scan.startPoint()
+scan = Scanner()
+scan.getScannedResult()
 
 
