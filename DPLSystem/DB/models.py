61546d785684c370f7a1437dca955ec14212dc38
Arian <avafadar@sfu.ca>
2020-08-06 17:15:45 -0700

    Updated DB with Constraints. Requires Verfication

--- DPLSystem/DB/models.py
+++ DPLSystem/DB/models.py
@@ -1,24 +1,25 @@
 from . import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import CheckConstraint
 
 
 class User(Base):
     __tablename__ = "user"
     uid = Column(Integer, primary_key=True)
-    email = Column(String(120))
-    first_name = Column(String(20))
-    last_name = Column(String(20))
-    password = Column(String(20))
-    phone_number = Column(String(16))
-    postal_code = Column(String(10))
-    Dplstation = Column(Integer, ForeignKey('dplstations.station_id'))
+    email = Column(String(120), nullable=False)
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    password = Column(String(20), nullable=False)
+    phone_number = Column(String(16), nullable=False)
+    postal_code = Column(String(10), nullable=False)
+    Dplstation = Column(Integer, ForeignKey('dplstations.station_id'), CheckConstraint('Dplstation > 0'))
     users_deliveries = relationship('Deliveries', backref='User', lazy=True)
 
 class DPLstations(Base):
     __tablename__ = "dplstations"
-    station_id = Column(Integer, primary_key=True)
-    city = Column(String(20))
+    station_id = Column(Integer, primary_key=True, CheckConstraint('station_id > 999'))
+    city = Column(String(20), nullable=False)
     def __repr__(self):
         return f"email={self.email}, user_del={self.users_deliveries}"
 
