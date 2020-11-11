13a937e908e2d0ce0e51feae0de26455a834aafe
chaoqund <chaoqun_ding@sfu.ca>
2020-11-11 15:42:43 -0800

    Upload New File

---
+++ faceSmileEyeDetection.py
@@ -0,0 +1,68 @@
+
+
+import numpy as np
+import cv2
+import time
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+i = 0
+while i != 1:
+    ret, img = cap.read()
+    #img = cv2.flip(img, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    if i==0:
+        print("unable to detect face")
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        eyes = eyeCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=5,
+            minSize=(5, 5),
+            )
+        
+#        for (ex, ey, ew, eh) in eyes:
+#            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+               
+        
+        smile = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=15,
+            minSize=(25, 25),
+            )
+        
+#        for (xx, yy, ww, hh) in smile:
+#            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
+        
+        cv2.imshow('video', img)
+        if len(faces)!=0 and len(eyes)==0:
+            print("unable to detect eyes")
+        if len(faces)!=0 and len(smile)==0:
+            print("unable to detect mouth")
+        if len(faces)!=0 and len(eyes)!=0 and len(smile)!=0:
+            face = cv2.imwrite(filename="face.jpg", img=img)
+            print("face captured")
+            i = 1
+            break
+
+cap.release()
+cv2.destroyAllWindows()
