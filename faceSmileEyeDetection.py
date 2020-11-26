9bf74f5524268996c9aea117d1c39e0da25bf258
chaoqund <chaoqun_ding@sfu.ca>
2020-11-26 15:56:22 -0800

    Replace faceSmileEyeDetection.py

--- faceSmileEyeDetection.py
+++ faceSmileEyeDetection.py
@@ -1,4 +1,3 @@
-
 import cv2
 
 faceCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
@@ -6,8 +5,8 @@ eyeCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/
 smileCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/cv2/data/haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640) 
-cap.set(4,480) 
+cap.set(3,640)
+cap.set(4,480)
 i = 0
 while i != 1:
     ret, img = cap.read()
@@ -19,8 +18,8 @@ while i != 1:
         minSize=(30, 30)
     )
 
-    if i==0:
-        print("unable to detect face")
+#     if i==0:
+#         print("unable to detect face")
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -32,8 +31,7 @@ while i != 1:
             scaleFactor= 1.5,
             minNeighbors=5,
             minSize=(5, 5),
-            )
-        
+            )       
         
         smile = smileCascade.detectMultiScale(
             roi_gray,
@@ -42,17 +40,19 @@ while i != 1:
             minSize=(25, 25),
             )
         
-        
         cv2.imshow('video', img)
-        if len(faces)!=0 and len(eyes)==0:
-            print("unable to detect eyes")
-        if len(faces)!=0 and len(smile)==0:
-            print("unable to detect mouth")
+#         if len(faces)!=0 and len(eyes)==0:
+#             print("unable to detect eyes")
+#         if len(faces)!=0 and len(smile)==0:
+#             print("unable to detect mouth")
         if len(faces)!=0 and len(eyes)!=0 and len(smile)!=0:
             face = cv2.imwrite(filename="face.jpg", img=img)
             print("face captured")
             i = 1
             break
     cv2.imshow('video', img)
+#     k = cv2.waitKey(30) & 0xff
+#     if k == 27:
+#         break
 cap.release()
 cv2.destroyAllWindows()
