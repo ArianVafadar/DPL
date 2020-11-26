4acc36e615a878e5b0bf6be8a85e36409bbb482a
chaoqund <chaoqun_ding@sfu.ca>
2020-11-25 20:56:50 -0800

    Update faceSmileEyeDetection.py

--- faceSmileEyeDetection.py
+++ faceSmileEyeDetection.py
@@ -1,21 +1,16 @@
 
-
-import numpy as np
 import cv2
-import time
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
-eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
-smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
+faceCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/cv2/data/haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('/home/pi/.local/lib/python3.7/site-packages/cv2/data/haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+cap.set(3,640) 
+cap.set(4,480) 
 i = 0
 while i != 1:
     ret, img = cap.read()
-    #img = cv2.flip(img, -1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
         gray,
@@ -39,9 +34,6 @@ while i != 1:
             minSize=(5, 5),
             )
         
-#        for (ex, ey, ew, eh) in eyes:
-#            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-               
         
         smile = smileCascade.detectMultiScale(
             roi_gray,
@@ -50,8 +42,6 @@ while i != 1:
             minSize=(25, 25),
             )
         
-#        for (xx, yy, ww, hh) in smile:
-#            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
         
         cv2.imshow('video', img)
         if len(faces)!=0 and len(eyes)==0:
@@ -63,6 +53,6 @@ while i != 1:
             print("face captured")
             i = 1
             break
-
+    cv2.imshow('video', img)
 cap.release()
 cv2.destroyAllWindows()
